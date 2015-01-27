8ce17e9a3e0ab087d23f831bf8611d16b1155a09
alekseenkoss <alekseenkoss@gmail.com>
2015-01-27 17:35:13 +0600

    fix paht

--- lib/amorail/engine.rb
+++ lib/amorail/engine.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Amorail
   class Engine < Rails::Engine
     rake_tasks do
-      load "amorail/tasks/class_generator.rake"
+      load "tasks/class_generator.rake"
     end
   end
 end
