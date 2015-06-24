3ae99fe25246067afac20e26fc0bb5ca4c41f7e6
Alex Baidan <howtwizer@gmail.com>
2015-06-24 13:54:17 +0300

    lp_db_id

--- lib/amorail/entities/contact.rb
+++ lib/amorail/entities/contact.rb
@@ -11,6 +11,7 @@ module Amorail
     amo_property :email, enum: 'WORK'
     amo_property :phone, enum: 'MOB'
     amo_property :position
+    amo_property :lp_db_id
 
     validates :name, presence: true
 
